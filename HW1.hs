e69ff857d2eb75a2ef871fb41f0849d96e369ceb
Sean Sweet <sweet.sean42@gmail.com>
2022-04-11 15:47:00 -0500

    changed isIn and areIn to match new requirements

--- HW1.hs
+++ HW1.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE FlexibleInstances #-}
 isAscending :: Ord a => [a] -> Bool
 isAscending [] = True
 isAscending [_] = True
@@ -17,29 +16,18 @@ isSorted x = isAscending x || isDescending x
 areSorted :: Ord a => [[a]] -> Bool
 areSorted = all isSorted
 
-class MyType a where
-    isIn :: Int -> a -> Bool
+isIn :: Eq a => a -> [a] -> Bool
+isIn _ [] = False
+isIn n (x:xs) = n == x || isIn n xs
 
-instance MyType [Int] where
-    isIn n xs = n `elem` xs
-
-instance MyType [[Int]] where
-    isIn n xs = any (isIn n) xs
-
-class MyType2 a where
-    areIn :: [Int] -> a -> Bool
-
-instance MyType2 [Int] where
-    areIn ns xs = all (`elem` xs) ns
-
-instance MyType2 [[Int]] where
-    areIn ns xs = all (`isIn` xs) ns
+areIn :: Eq a => [a] -> [a] -> Bool
+areIn ns xs = all (`isIn` xs) ns
 
 main = do
     print(isSorted [1,2,3,4,5])
     print(isSorted [5,4,3,2,1])
     print(isSorted [1,3,2,4,5])
     print(areSorted [[1,2,3,4,5],[5,4,3,2,1],[1,2,3,4,5]])
-    print(isIn 15 ([[1,2,3,4,5], [10,20,30,40,50]] :: [[Int]]))
-    print(isIn 1 ([1,2,3,4,5] :: [Int]))
-    print(areIn [1,2,10] ([[1,2,3,4,5], [10,20,30,40,50]] :: [[Int]]))
+    print(isIn 1 [1,2,3,4,5])
+    print(areIn [1,2,5] [1,2,3,4,5])
+    print(areIn [] [1,2,3,4,5])
