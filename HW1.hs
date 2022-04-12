c2fc525d08e2ff1a72c892f107ec40029c3d6af9
Sean Sweet <smsweet@crimson.ua.edu>
2022-04-12 17:11:01 -0500

    removed main method for submission

--- HW1.hs
+++ HW1.hs
@@ -22,12 +22,3 @@ isIn n (x:xs) = n == x || isIn n xs
 
 areIn :: Eq a => [a] -> [a] -> Bool
 areIn ns xs = all (`isIn` xs) ns
-
-main = do
-    print(isSorted [1,2,3,4,5])
-    print(isSorted [5,4,3,2,1])
-    print(isSorted [1,3,2,4,5])
-    print(areSorted [[1,2,3,4,5],[5,4,3,2,1],[1,2,3,4,5]])
-    print(isIn 1 [1,2,3,4,5])
-    print(areIn [1,2,5] [1,2,3,4,5])
-    print(areIn [] [1,2,3,4,5])
