122af10ef9164ba80d921f9553f12c5a4ff5e2c5
= <=>
2022-04-25 16:47:16 -0500

    eta reduction

--- HW4.hs
+++ HW4.hs
@@ -2,33 +2,27 @@
 
 --Given a list of integers, return a list of the integers, omitting any that are less than 0
 noNeg:: [Int] -> [Int]
-noNeg [] = []
-noNeg x = filter (>=0) x
+noNeg = filter (>=0)
 
 --Given a list of non-negative integers, return a list of those numbers except omitting any that end with 9
 no9:: [Int] -> [Int]
-no9 [] = []
-no9 x = filter (\x -> mod x 10 /= 9) x
+no9 = filter (\x -> mod x 10 /= 9)
 
 --Given a list of integers, return a list of those numbers, omitting any that are between 13 and 19 inclusive
 noTeen:: [Int] -> [Int]
-noTeen [] = []
-noTeen x = filter (\x -> x < 13 || x > 19) x
+noTeen = filter (\x -> x < 13 || x > 19)
 
 --Given a list of strings, return a list of the strings, omitting any string that contains a "z"
 noZ:: [String] -> [String]
-noZ [] = []
-noZ x = filter (\x -> 'z' `notElem` x) x
+noZ = filter (\x -> 'z' `notElem` x)
 
 --Given a list of strings, return a list of the strings, omitting any string length 4 or more
 noLong:: [String] -> [String]
-noLong [] = []
-noLong x = filter (\x -> length x < 4) x
+noLong = filter (\x -> length x < 4)
 
 --Given a list of strings, return a list of the strings, omitting any string length 3 or 4
 no34:: [String] -> [String]
-no34 [] = []
-no34 x = filter (\x -> length x /= 3 && length x /= 4) x
+no34 = filter (\x -> length x /= 3 && length x /= 4)
 
 main = do
     print $ noNeg [1,-2,3,4,5,-1,-2,-3,-4,-5]
