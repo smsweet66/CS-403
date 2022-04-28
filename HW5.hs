aacd7c8072183778bd2f21ce304c1359649ef3b3
Sean Sweet <sweet.sean42@gmail.com>
2022-04-27 20:09:33 -0500

    finished HW5

--- HW5.hs
+++ HW5.hs
@@ -1,16 +1,16 @@
 --Author: Sean Sweet
 
 --Given a list of strings, return a list where each string has "y" added at its end, omitting any resulting strings that contain "yy" as a substring anywhere
-
-containsYY :: [Char] -> Bool
-containsYY [] = False
-containsYY [_] = False
-containsYY (x:y:xs) = x == 'y' && y == 'y' || containsYY xs
-
 noYY :: [String] -> [String]
-noYY = filter (\x -> containsYY x) . map (++ "y")
+noYY = filter (\x -> not $ containsYY x) . map (++ "y") where
+    containsYY [] = False
+    containsYY [_] = False
+    containsYY (x:y:xs) = x == 'y' && y == 'y' || containsYY (y:xs)
 
-main = do
-    print $ noYY ["a", "b", "c"]
-    print $ noYY ["a", "b", "cy"]
-    print $ noYY ["xx", "ya", "zz"]
+--Given a list of non-negative integers, return a list of those numbers multiplied by 2, omitting any of the resulting numbers that end in 2
+two2 :: [Int] -> [Int]
+two2 = filter (\x -> x `mod` 10 /= 2) . map (*2)
+
+--Given a list of integers, return a list of those numbers squared and the product added to 10, omitting any of the resulting numbers that end in 5 or 6
+square56 :: [Int] -> [Int]
+square56 = filter (\x -> x `mod` 10 /= 5 && x `mod` 10 /= 6) . map (\x -> x*x + 10)
